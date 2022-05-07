9ca63eafa7476c8007b94b03336e8d19cfb3773f
bin <bin@test.com>
2022-05-07 15:26:53 +0800

    add test

--- src/test/java/com/test/git/GitTest.java
+++ src/test/java/com/test/git/GitTest.java
@@ -2,8 +2,12 @@ package com.test.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+
         System.out.println("hello world2");
         System.out.println("hello git3");
+        System.out.println("hellot test1");
+        System.out.println("hello git4");
+        System.out.println("test5");
+
     }
 }
